3df64e04c1450099519edb3fc87a896328e5ba47
mathewnicholls <mathewnicholls@yandex.com>
2024-07-29 16:00:19 +0100

    refactor to improve readability and remove duplication

--- client/movement.go
+++ client/movement.go
@@ -14,41 +14,16 @@ type TrainCompanySub struct {
 	SubChan chan movement.Body
 }
 
-func (nr *NetworkRailClient) SubAllTrainMovement() (chan movement.Body, error) {
+func (nr *NetworkRailClient) SubAllTrainMovement() (<-chan movement.Body, error) {
 	sub, err := nr.stompConnection.Subscribe("/topic/TRAIN_MVT_ALL_TOC", stomp.AckAuto)
-
 	if err != nil {
-		return nil, fmt.Errorf("could not subscribe to All Train Movements topic: %v", err)
+		return nil, fmt.Errorf("could not subscribe to All Train Movements topic: %w", err)
 	}
 
 	nr.allTrainMovementsChan = make(chan movement.Body, 100)
 
-	nr.wg.Add(1)
-	go func() {
-		defer func() {
-			nr.wg.Done()
-			close(nr.allTrainMovementsChan)
-		}()
+	go nr.handleSubscription(sub, nr.allTrainMovementsChan)
 
-		for {
-			select {
-			case msg := <-sub.C:
-				var message []movement.Message
-				err = json.Unmarshal(msg.Body, &message)
-				if err != nil {
-					fmt.Printf("Error unmarshaling message: %v\n", err)
-					continue
-				}
-				for _, m := range message {
-					data := movement.Convert(m.Body, m.Header.MsgType)
-					nr.allTrainMovementsChan <- data
-				}
-			case <-nr.ctx.Done():
-				log.Println("All Train Movements Ending...")
-				return
-			}
-		}
-	}()
 	return nr.allTrainMovementsChan, nil
 }
 
@@ -56,50 +31,56 @@ func (nr *NetworkRailClient) SubMultiTrainCompanyMovements(operators []model.Tra
 	if len(operators) == 0 {
 		return nil, fmt.Errorf("no operators provided")
 	}
+
 	var trainCompanySubs []*TrainCompanySub
 	for _, operator := range operators {
-		trainCompanySubs = append(trainCompanySubs, &TrainCompanySub{
+		trainCompanySub := &TrainCompanySub{
 			TrainOperator: operator,
 			SubChan:       make(chan movement.Body, 50),
-		})
-	}
+		}
+		trainCompanySubs = append(trainCompanySubs, trainCompanySub)
 
-	for i, trainCompanySub := range trainCompanySubs {
-		topic := fmt.Sprintf("/topic/TRAIN_MVT_%s_TOC", trainCompanySub.TOC)
+		topic := fmt.Sprintf("/topic/TRAIN_MVT_%s_TOC", operator.TOC)
 		sub, err := nr.stompConnection.Subscribe(topic, stomp.AckAuto)
 		if err != nil {
-			return nil, fmt.Errorf("could not subscribe to Train Movements topic: %v", err)
+			return nil, fmt.Errorf("could not subscribe to Train Movements topic for %s: %w", operator.Name, err)
 		}
+
 		nr.companyMovementsChans = append(nr.companyMovementsChans, trainCompanySub.SubChan)
 
-		nr.wg.Add(1)
-		go func(chanIndex int, companyName string) {
-			log.Println("Subscribing to Train Movements Topic:", topic)
-			defer func() {
-				close(nr.companyMovementsChans[i])
-				nr.wg.Done()
-			}()
-
-			for {
-				select {
-				case msg := <-sub.C:
-					var message []movement.Message
-					err := json.Unmarshal(msg.Body, &message)
-					if err != nil {
-						fmt.Printf("Error unmarshaling message: %v\n", err)
-						continue
-					}
-					for _, m := range message {
-						log.Println(msg.Body)
-						data := movement.Convert(m.Body, m.Header.MsgType)
-						nr.companyMovementsChans[i] <- data
-					}
-				case <-nr.ctx.Done():
-					log.Printf("Ending movement sub for train company %s", companyName)
-					return
-				}
-			}
-		}(i, trainCompanySub.Name)
+		go nr.handleSubscription(sub, trainCompanySub.SubChan)
 	}
+
 	return trainCompanySubs, nil
+}
+
+func (nr *NetworkRailClient) handleSubscription(sub *stomp.Subscription, movementChan chan<- movement.Body) {
+	defer nr.wg.Done()
+	defer close(movementChan)
+
+	nr.wg.Add(1)
+	log.Printf("Subscribing to Train Movements Topic: %s", sub.Destination())
+
+	for {
+		select {
+		case msg := <-sub.C:
+			nr.processMessage(msg, movementChan)
+		case <-nr.ctx.Done():
+			log.Printf("Ending movement sub for %s", sub.Destination())
+			return
+		}
+	}
+}
+
+func (nr *NetworkRailClient) processMessage(msg *stomp.Message, movementChan chan<- movement.Body) {
+	var messages []movement.Message
+	if err := json.Unmarshal(msg.Body, &messages); err != nil {
+		log.Printf("Error unmarshaling message: %v", err)
+		return
+	}
+
+	for _, m := range messages {
+		data := movement.Convert(m.Body, m.Header.MsgType)
+		movementChan <- data
+	}
 }
